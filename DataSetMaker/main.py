e32b9fc3f3d137fec539ec874311f46544bb76c4
Esad Yusuf Atik <esadyusufatik@gmail.com>
2020-04-11 00:24:00 +0300

    created

---
+++ DataSetMaker/main.py
@@ -0,0 +1,34 @@
+import json
+import parser
+import time
+
+fileName = "/Users/fuat/Desktop/esad/coding/StockAI/DataSetMaker/deneme.json"
+
+stocks = ["YUNSA","EREGL","MAKTK","KRDMD","THYAO","TUPRS","PETKM","ALKIM","ISCTR","GARAN","TCELL","ARCLK","EKGYO","ASELS","HEKTS"]
+
+try:
+    while True:
+        json_read = open(fileName,"r")
+        data = json.load(json_read)
+        json_read.close()
+
+        for i in range(0,len(stocks)-2):
+            stockName = stocks[i]
+            print(stockName)
+            currentPrice, table = parser.parse(stockName)
+            if currentPrice != -1:
+                newParse = {
+                    "kademeDeğerleri":table,
+                    "stockPrice":currentPrice
+                }
+                data.get("stocks")[i].get("parses").append(newParse)
+        
+        json_write = open(fileName,"w")
+        json.dump(data,json_write,indent = 4)
+        json_write.close()
+        time.sleep(900)
+        
+except KeyboardInterrupt:
+    print("User stopped the program.")
+    exit()
+
---
+++ DataSetMaker/parser.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+errorCounter = 0
+
+def parse(stockName):
+    global errorCounter
+    page = requests.get("http://graph.foreks.com/grafik/webfx/line3djson.jsp?hisse="+stockName)
+    print(page.status_code)
+
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.content, 'html.parser')
+        currentVal = float(soup.findAll("td", {"style" : "color:#ff0000"})[0].get_text().replace(",","."))
+        data = soup.find_all("tr")
+        data = data[1:len(data)]
+        table = []
+        for item in data:
+            values = item.get_text().split('\n')
+            values = [values[1],values[3]]
+            values = [i.replace(",",".") for i in values]
+            values = [i.replace("%","") for i in values]
+            values = [float(i) for i in values[0:2]]
+            table.append(values)
+        return currentVal,table
+        
+    else:
+        errorCounter+=1
+        print(errorCounter)
+        if errorCounter == 2:
+            errorCounter = 0
+            return -1,-1
+        else:
+            return parse(stockName)
