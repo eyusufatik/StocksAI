864c160040e3504b07f737ba5ab27e46e5ec68ba
Esad Yusuf Atik <esadyusufatik@gmail.com>
2020-04-13 16:08:13 +0300

    realized comparing the new parsing with the  last parsing was wrong and unnecessary so I got rid of it

--- DataSetMaker/main.py
+++ DataSetMaker/main.py
@@ -6,7 +6,7 @@ fileName = "13.04.2020.json"
 #fileName = "deneme.json"
 stocks = ["HEKTS","YUNSA","EREGL","MAKTK","KRDMD","THYAO","TUPRS","PETKM","ALKIM","ISCTR","GARAN","TCELL","ASELS","ARCLK","EKGYO"]
 
-lastKademe = []
+#lastValues = [[[]]]*15
 try:
     while True:
         json_read = open(fileName,"r")
@@ -15,10 +15,11 @@ try:
 
         for i in range(0,len(stocks)):
             stockName = stocks[i]
-            print(stockName)
+            print(stockName,end=" ",)
             currentPrice, table = parser.parse(stockName)
-            if currentPrice != -1 and lastKademe != table:
-                lastKademe=table
+            if currentPrice != -1: #and lastValues[i] != table:
+                #print("new reading detected")
+                #lastValues[i]=table
                 newParse = {
                     "kademeDeğerleri":table,
                     "stockPrice":currentPrice
@@ -30,7 +31,7 @@ try:
         json_write = open(fileName,"w")
         json.dump(data,json_write,indent = 4)
         json_write.close()
-        print("================================================")
+        print("===========================================================")
         time.sleep(30)
         
 except KeyboardInterrupt:
